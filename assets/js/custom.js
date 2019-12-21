904a9b648db0c96b279192bf67611ebbb2c950d1
noxx22 <nonda77@yahoo.com>
2019-12-21 16:55:51 +0300

    button "TO-TOP" added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,31 @@
 $(function () {
 
-    $('.portfolio-item:odd').addClass('item--odd')
+    $('.portfolio-item:odd').addClass('item--odd');
+
+    //======SCROLL TO TOP==================================
+
+    var $btnTop = $('.btn-top');
+
+    $(window).on('scroll', function () {
+
+        if ($(window).scrollTop() >= 100) {
+
+            $btnTop.fadeIn();
+
+        } else {
+
+            $btnTop.fadeOut();
+
+        }
+
+    });
+
+    $btnTop.on('click', function () {
+
+        $('html, body').animate({
+            scrollTop: 0
+        }, 900)
+
+    });
 
 });
