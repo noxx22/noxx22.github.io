e1d7a475dbb9ad5d1527d373b44fbcbd287a804d
noxx22 <nonda77@yahoo.com>
2020-01-11 18:15:56 +0300

    small improvments

--- web-development-project/assets/js/script.js
+++ web-development-project/assets/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   // Ждём загрузки страницы
   $(".tabs_menu li").click(function () {
     // Событие нажатия на элемент меню вкладок
-    if (! $(this).hasClass("active")) {
+      if (! $(this).hasClass("active")) {
       // Проверка, не нажали ли мы на уже активный пункт
       var i = $(this).index(); // Получаем порядковый номер нажатого пункта, отстче идет от 0 (0,1,2)
 
@@ -15,51 +15,51 @@ $(document).ready(function () {
 
       $(this).addClass("active"); // Добавляем нажатому пункту меню активный класс
 
-      $($(".tabs").children(".info")[ i ]).fadeIn(1000).addClass("active"); // Показываем и добавляем активный
+          $($(".tabs").children(".info")[ i ]).fadeIn(1000).addClass("active"); // Показываем и добавляем активный
       // класс соответствующему контейнеру
     }
   }); //======TEAM-CAROUSEL======
 
   $('.team-carousel').owlCarousel({
-    autoplay          : true,
+      autoplay        : true,
     autoplayHoverPause: true,
-    smartSpeed        : 1000,
-    loop              : true,
-    nav               : false,
-    margin            : 30,
-    dots              : true,
-    responsiveClass   : true,
-    autoWidth         : false,
-    dotsContainer     : '.team-carousel-pager',
-    dotClass          : 'team-dot',
-    responsive        : {
-      0  : {
-        items : 1,
-        margin: 10,
-        dots  : true
+      smartSpeed      : 1000,
+      loop            : true,
+      nav             : false,
+      margin          : 30,
+      dots            : true,
+      responsiveClass : true,
+      autoWidth       : false,
+      dotsContainer   : '.team-carousel-pager',
+      dotClass        : 'team-dot',
+      responsive      : {
+          0: {
+              items: 1,
+        margin     : 10,
+              dots : true
       },
       361: {
-        items : 1,
-        margin: 10,
-        dots  : true
+          items: 1,
+        margin : 10,
+          dots : true
       },
       576: {
-        items : 2,
-        margin: 15,
-        dots  : true
+          items: 2,
+        margin : 15,
+          dots : true
       },
       853: {
-        items : 3,
-        dots  : true,
-        loop  : true,
-        margin: 15
+          items: 3,
+          dots : true,
+          loop : true,
+        margin : 15
       }
     }
   }); //============SCROLL-TO-ID=============
 
   $('.nav-link').mPageScroll2id({
     forceSingleHighlight: false,
-    offset              : - '70'
+      offset            : - '70'
   }); //======SCROLL TO TOP==================================
 
   var $btnTop = $('.btn-top');
